c4bde4bc354c786978b30c4304d0276d95469a60
farukmgm <farukmaisikeli@gmail.com>
2026-09-07 14:42:40 +0100

    Add remember_me field to LoginForm

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, BooleanField
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from app.models import User
@@ -39,6 +39,7 @@ class LoginForm(FlaskForm):
     """User login form"""
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Rememner Me')
     submit = SubmitField('Login')
 
 class ReflectionForm(FlaskForm):
