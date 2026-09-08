49fea0f82533a955060882875123b9354069f067
farukmgm <farukmaisikeli@gmail.com>
2026-09-08 11:00:53 +0100

    Correct imports in forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
-from flask_wtf import FlaskForm, BooleanField
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from app.models import User
 
